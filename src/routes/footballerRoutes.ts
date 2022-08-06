2600c0a932de7e22e7b54c2a00c2790951b73940
Chinmay Karmokar <chinmaykarmokar@gmail.com>
2022-08-06 21:32:48 +0530

    Added two more endpoints to the Footballers API. Done minor refactoring.

--- src/routes/footballerRoutes.ts
+++ src/routes/footballerRoutes.ts
@@ -6,12 +6,15 @@ import { Footballers } from "../Entities/Footballers";
 // Import DB config
 import connectDB from "../../config/ormconfig";
 
+const app = express();
+app.use(express.json());
+
 const router = Router();
 
 router.post("/", async (req: Request, res: Response) => {
     try {
         if (req.body.length != 0) {
-            await Footballers.insert(req.body);
+            await connectDB.getRepository(Footballers).insert(req.body);
 
             res.json({
                 message: "Values inserted successfully!"
@@ -31,9 +34,32 @@ router.post("/", async (req: Request, res: Response) => {
 })
 
 router.get("/", async (req: Request, res: Response) => {
-    await Footballers.find().then((data) => {
-        res.json(data);
-    })
+    const data = await connectDB.getRepository(Footballers).find();
+    res.json(data);
+})
+
+router.put("/:id", async (req: Request, res: Response) => {
+    const footballer =  await connectDB.getRepository(Footballers).findOne({where: {id: parseInt(req.params.id)}})
+
+    if (footballer != null) {
+        connectDB.getRepository(Footballers).merge(footballer, req.body);
+
+        await connectDB.getRepository(Footballers).save(footballer);
+
+        res.json({
+            message: "Values updated successfully!"
+        })
+
+    }
+    else {
+        res.json({
+            error: "Values could not be updated"
+        })
+    }
+})
+
+router.delete("/:id", async (req: Request, res: Response) => {
+    await connectDB.getRepository(Footballers).delete(req.params.id);
 })
 
 export default router;
